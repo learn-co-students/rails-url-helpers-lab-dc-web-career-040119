da636809c6f620e6e601f4e5882a066d9749a53f
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-04-29 13:03:39 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+paul= Student.find_or_create_by(last_name: "Nicholsen", first_name: "Paul", active: false)
+peter= Student.find_or_create_by(last_name: "Nicholsen", first_name: "Peter", active: false)
+pat= Student.find_or_create_by(last_name: "Nicholsen", first_name: "Pat", active: false)
+patience= Student.find_or_create_by(last_name: "Nicholsen", first_name: "Patience", active: false)
+pete= Student.find_or_create_by(last_name: "Nicholsen", first_name: "Pete", active: false)
